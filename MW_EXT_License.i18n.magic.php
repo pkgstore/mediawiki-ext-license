8fffddcc4b26a8f2a4b08b16d2a91f3c019f1eac
KaiKimera <mail@kai.kim>
2024-02-01 04:31:29 +0000

    2024-02-01 04:31:29

--- MW_EXT_License.i18n.magic.php
+++ MW_EXT_License.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @license MIT
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['en'] = [
   'license' => [0, 'license'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['ru'] = [
   'license' => [0, 'license'],
